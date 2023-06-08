01f2d0e6dd04077295d1248fbb95d8ea8694d2d8
vineet.kothari <vineet.kothari@illumio.com>
2023-06-07 21:08:33 -0700

    Adding dependencies & setupnodeevents

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,21 @@
+const webpackPreprocessor = require('@cypress/webpack-preprocessor')
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      const webpackDefaults = webpackPreprocessor.defaultOptions;
+      webpackDefaults.webpackOptions.module.rules.push({
+        test: /\.csv$/,
+        loader: 'csv-loader',
+        options: {
+          dynamicTyping: true,
+          header: true,
+          skipEmptyLines: true,
+        },
+      });
+      on('file:preprocessor', webpackPreprocessor(webpackDefaults));
     },
   },
 });
